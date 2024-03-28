5de0f29456e0fec6e111c8c5cb1c7aafa76739ab
HooriaAli <162888582+HooriaAli@users.noreply.github.com>
2024-03-28 11:11:35 +0500

    Add files via upload

---
+++ Assignment no.1.py
@@ -0,0 +1,33 @@
+# to find number is positive, negative or zero.
+num = -7
+if num > 0:
+    print("number is positive")
+elif num < 0:
+    print("number is negative")
+else:
+    print("0")
+# to find the largest among three numbers
+a = 2
+b = 4
+c = 6
+if a>b and a>c:
+    print("largest number is",a)
+elif b>a and b>c:
+    print("largest number is", b)
+else:
+    print("largest number is",c)
+# to find if the year isleap or not
+year = 2023
+if year%4 ==0:
+    print("leap year")
+else:
+    print("not leap year")
+# to check if given character is vowel or consonant
+x= 'h'
+if x=='a' or x=='e' or x=='i' or x=='o' or x=='u':
+    print(x, 'is vowel')
+else:
+    print('consonant')
+
+
+
